a7a4495f6a0a28fea02ec9c988b29c55906d0780
Awakee <screamm97@gmail.com>
2017-10-05 16:02:54 +0300

    Added hot reload

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,19 +1,38 @@
-var gulp = require('gulp'),
-    minjs = require('gulp-uglify');
-    mincss = require('gulp-clean-css');
-    suffix = require('gulp-rename');
-    minimg = require('gulp-imagemin');
+var gulp = require('gulp');
+var minjs = require('gulp-uglify');
+var mincss = require('gulp-clean-css');
+var suffix = require('gulp-rename');
+var minimg = require('gulp-imagemin');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var paths = {
+      html:['index.html'],
+      css:['css/style.css'],
+      js:['js/registration.js']
+    };
+    
+gulp.task('html', function(){
+  gulp.src(paths.html)
+  .pipe(reload({stream:true}));
+});
+
+gulp.task('css', function(){
+  gulp.src(paths.css)
+  .pipe(reload({stream:true}))
+});
+
+gulp.task('js', function(){
+  gulp.src(paths.js)
+  .pipe(reload({stream:true}))
+});
+
 
 gulp.task('minjs', function () {
   gulp.src('js/registration.js')
   .pipe(minjs())
   .pipe(suffix({suffix: '.min'}))
   .pipe(gulp.dest('build/js/'));
-});
-
-gulp.task('transferjquery', function(){
-  gulp.src('js/jquery-3.2.1.min.js')
-  .pipe(gulp.dest('build/js'))
 });
 
 gulp.task('mincss', function() {
@@ -29,7 +48,28 @@ gulp.task('minimg', function(){
   .pipe(gulp.dest('build/img'))
 });
 
+gulp.task('transferjquery', function(){
+  gulp.src('js/jquery-3.2.1.min.js')
+  .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('watcher',function(){
+  gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.js, ['js']);
+  gulp.watch(paths.css, ['css']);
+});
 
 gulp.task('build', ['minjs', 'mincss', 'transferjquery', 'minimg']);
+gulp.task('default', ['watcher', 'browserSync']);
 
+gulp.task('browserSync', function(){
+  browserSync({
+    server: {
+      baseDir: "./"
+    },
+    port: 8080,
+    open: true,
+    notify: false
+  });
+});
 
