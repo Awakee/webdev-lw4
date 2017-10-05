789013e004176124d2b9121cfe064d9558ec768d
Awakee <screamm97@gmail.com>
2017-10-05 15:04:37 +0300

    Added suffix / transfer jquery to build

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,26 +1,28 @@
 var gulp = require('gulp'),
     minjs = require('gulp-uglify');
     mincss = require('gulp-clean-css');
+    suffix = require('gulp-rename');
 
 gulp.task('minjs', function () {
   gulp.src('js/registration.js')
   .pipe(minjs())
+  .pipe(suffix({suffix: '.min'}))
   .pipe(gulp.dest('build/js/'));
 });
 
-
+gulp.task('transferjquery', function(){
+  gulp.src('js/jquery-3.2.1.min.js')
+  .pipe(gulp.dest('build/js'))
+});
 
 gulp.task('mincss', function() {
   return gulp.src('css/style.css')
   .pipe(mincss())
+  .pipe(suffix({suffix: '.min'}))
   .pipe(gulp.dest('build/css/'))
 });
 
-gulp.task('html', function(){
-  gulp.src('index.html')
-  .pipe(gulp.dest('build'))
-});
 
-gulp.task('build', ['minjs', 'mincss', 'html']);
+gulp.task('build', ['minjs', 'mincss', 'transferjquery']);
 
 
